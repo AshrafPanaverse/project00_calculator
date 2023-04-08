9e449ce28e5f332ddafd7ea3441b5af17e0aad57
Ashraf Hussain <125198812+AshrafPanaverse@users.noreply.github.com>
2023-04-08 13:56:04 +0500

    'calculator

---
+++ calculator.ts
@@ -0,0 +1,17 @@
+const add= (num1:any,num2:any)=>{
+    return (num1+num2);
+}
+
+const subtract=(num1:any,num2:any)=>{
+    return (num1-num2);
+}
+
+const multiply=(num1:any,num2:any)=>{
+    return (num1*num2);
+}
+
+const division=(num1:any,num2:any)=>{
+    return (num1/num2);
+}
+
+export default {add,subtract,multiply,division};
---
+++ index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import calculator from './calculator.js'
+import inquirer from 'inquirer';
+import questions from './questions.js';
+
+const prompt=await inquirer.prompt(questions);
+
+let result: number;
+
+switch (prompt.operation) {
+    case '+':
+      result = calculator.add(prompt.num1, prompt.num2);
+      break;
+    case '-':
+      result = calculator.subtract(prompt.num1,prompt.num2);
+      break;
+    case '*':
+      result = calculator.multiply(prompt.num1, prompt.num2);
+      break;
+    case '/':
+      result = calculator.division(prompt.num1, prompt.num2);
+      break;
+    default:
+      throw new Error('Invalid operation');
+  }
+
+console.log(result);
